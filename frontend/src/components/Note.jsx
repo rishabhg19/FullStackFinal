db77152d696097720bf98d0a088528bd37c22341
Rishabh Ganesh <70454763+rishabhg19@users.noreply.github.com>
2024-05-06 16:24:55 -0400

    Add files via upload

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Note = (props) => {
+    return (
+
+                <div className="note">
+                <h1>{props.title}</h1>
+                <p>{props.content}</p>
+                <button onClick={props.delete}>DELETE</button>
+            </div>
+    );
+}
+
+export default Note;
